acfacf4f78ffe6c51f51e93eed33f0e2a34b750d
EwBax <W0465255@campus.nscc.ca>
2023-02-20 14:23:32 -0400

    Initial Commit, layouts done

---
+++ app/src/main/java/com/ewbax/quizbuilder/ResultsActivity.java
@@ -0,0 +1,15 @@
+package com.ewbax.quizbuilder;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_results);
+    }
+
+}
